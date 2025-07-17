b2f07301a9503ba639e02c3dff95b1eea1f4711b
nicolasrperez <nicoroman20002007@gmail.com>
2025-07-18 00:59:38 +0200

    Add files via upload

--- libreria/conexion.php
+++ libreria/conexion.php
@@ -1,17 +1,21 @@
 <?php
-class Conexion extends mysqli{
+include_once 'config.php';
 
- public $enlace;
- 
- function __construct(){
-   //$this->enlace=mysql_connect(DB_HOST,DB_USER,DB_PASS);
-   //mysql_select_db(DB_NAME,$this->enlace);
-   $this->enlace=mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-    
-  
- }
- function __destruct(){
-   //mysql_close($this->enlace);
-   mysqli_close($this->enlace);
- }
+class Conexion {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = conectar();  // Llama a la función conectar() de config.php
+    }
+
+    public function getConexion() {
+        return $this->conn;
+    }
+
+    public function __destruct() {
+        if ($this->conn) {
+            $this->conn->close();
+        }
+    }
 }
+?>
